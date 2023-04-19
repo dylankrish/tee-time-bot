d3963ce94b877ff97a39c93128061b0efc23751a
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-04-18 23:14:08 -0400

    main

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import datetime
 def main():
     while True:
         now = datetime.datetime.now()
-        if now.hour == 10 and now.minute == 30:
+        if now.hour == 23 and now.minute == 14:
             print("It's 10:30!")
             break
         else:
             print("It's not 10:30 yet.")
+            print("Current time: " + str(now.hour) + ":" + str(now.minute))
             time.sleep(10)
 
 main()
