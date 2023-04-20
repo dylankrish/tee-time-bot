a3282eed8d417c793f30f669c21ab9231484b93c
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-04-20 09:57:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def getTeeTime():
     cookies = response.cookies
 
 
-    print(cookies)
-    print(response)
+    # print(cookies)
+    # print(response)
 
     # parse 'cookies' to get the value of the 'ASP.NET_SessionId' cookie and set it to the 'sessionid' variable
     cmspreferredculture = cookies['CMSPreferredCulture']
@@ -44,8 +44,8 @@ def getTeeTime():
     # we can do this by making a request to 'https://www.stoningtoncountryclub.com/login.aspx?ReturnUrl=%2fmember-central' page
 
     # TODO: Fix parantheses
-    headers1 = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                "Accept-Encoding": "gzip, deflate, br"
+    headers2 = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+                "Accept-Encoding": "gzip, deflate, br",
                 "Accept-Language": "en-US,en;q=0.5",
                 "Cache-Control": "max-age=0",
                 "Connection": "keep-alive",
@@ -60,14 +60,21 @@ def getTeeTime():
                 "Sec-Fetch-Site": "same-origin",
                 "Sec-Fetch-User": "?1",
                 "Sec-GPC": "1",
-                "Upgrade-Insecure-Requests:" "1",
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/
-                "sec-ch-ua": "Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/",
+                "sec-ch-ua": "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
                 "sec-ch-ua-mobile": "?0",
                 "sec-ch-ua-platform": "macOS"
                 }
 
-    response2 = requests.post(login_url2, data=login_data, headers=headers1)
+    response2 = requests.post(login_url2, data=login_data, headers=headers2)
+
+    cookies2 = response2.cookies
+
+    print(cookies2)
+
+    # parse 'cookies2' to get the value of the '.ASPXFORMSAUTH' cookie and set it to the 'aspxformsauth' variable
+    aspxformsauth = cookies2['.ASPXFORMSAUTH']
 
     # next we need to get the current user's ID
     # we can do this by making a GET request to 'https://www.stoningtoncountryclub.com/api/v1/GetCurrentUser'
@@ -77,29 +84,28 @@ def getTeeTime():
     booking_url = 'https://www.stoningtoncountryclub.com/api/v1/teetimes/CommitBooking/0'
 
     # TODO: Fix parantheses
-    headers2 = {"Accept": "application/json, text/plain, */*",
-                "Accept-Language:": "en-US,en;q=0.9",
+    headers3 = {"Accept": "application/json, text/plain, */*",
+                "Accept-Language": "en-US,en;q=0.9",
                 "Connection": "keep-alive",
                 "Content-Type": "application/json;charset=utf-8",
-                "Cookie": "ASP.NET_SessionId=" + sessionid,
+                "Cookie": "CMSPreferredCulture=" + cmspreferredculture + "; ASP.NET_SessionId=" + sessionid + "; test=ok; CMSCookieLevel=1000; CMSPreferredUICulture=en-US; .ASPXFORMSAUTH=" + aspxformsauth + "; JNS=" + jns,
                 "Origin": "https://www.stoningtoncountryclub.com",
                 "Referer": "https://www.stoningtoncountryclub.com/CMSModules/CHO/TeeTimes/TeeTimes.aspx",
                 "Sec-Fetch-Dest": "empty",
                 "Sec-Fetch-Mode": "cors",
                 "Sec-Fetch-Site": "same-origin",
                 "Sec-GPC": "1",
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-                "sec-ch-ua": "Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+                "sec-ch-ua": "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
                 "sec-ch-ua-mobile": "?0",
                 "sec-ch-ua-platform": "macOS",
                 # TODO: data raw
-                "--compressed"
                 }
 
 
-    response3 = requests.post(booking_url, headers=headers2)
+    # response3 = requests.post(booking_url, headers=headers3)
 
-    print(response.text)
+    # print(response.text)
     
     # Finally, let's book a tee time
     
