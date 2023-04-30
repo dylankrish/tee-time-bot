34ee9b9e463bb9673be5758f4cd8a4871b914210
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-04-29 21:29:34 -0400

    Redo Login2

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def getTeeTime():
         "Cookie": "CMSPreferredCulture=" + cmspreferredculture + "; ASP.NET_SessionId=" + sessionid + "; test=ok",
     }
 
-    response2 = requests.post(login_url2, data=login_data, headers=headers2)
+    response2 = requests.post(login_url, data=login_data, headers=headers2)
 
     cookies2 = response2.cookies
 
@@ -64,43 +64,6 @@ def getTeeTime():
     # parse 'cookies2' to get the value of the '.ASPXFORMSAUTH' cookie and set it to the 'aspxformsauth' variable
     aspxformsauth = cookies2['.ASPXFORMSAUTH']
     print(aspxformsauth)
-
-    # Login 2
-    # now we need to get the .ASPXFORMSAUTH cookie
-    # we can do this by making a request to 'https://www.stoningtoncountryclub.com/login.aspx?ReturnUrl=%2fmember-central' page
-
-    # # TODO: Fix parantheses
-    # headers2 = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    #             "Accept-Encoding": "gzip, deflate, br",
-    #             "Accept-Language": "en-US,en;q=0.5",
-    #             "Cache-Control": "max-age=0",
-    #             "Connection": "keep-alive",
-    #             "Content-Length": "2532",
-    #             "Content-Type": "application/x-www-form-urlencoded",
-    #             "Cookie": "CMSPreferredCulture=" + cmspreferredculture + "; ASP.NET_SessionId=" + sessionid + "; test=ok",
-    #             "Host": "www.stoningtoncountryclub.com",
-    #             "Origin": "https://www.stoningtoncountryclub.com",
-    #             "Referer": "https://www.stoningtoncountryclub.com/login.aspx?ReturnUrl=%2fmember-central",
-    #             "Sec-Fetch-Dest": "document",
-    #             "Sec-Fetch-Mode": "navigate",
-    #             "Sec-Fetch-Site": "same-origin",
-    #             "Sec-Fetch-User": "?1",
-    #             "Sec-GPC": "1",
-    #             "Upgrade-Insecure-Requests": "1",
-    #             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/",
-    #             "sec-ch-ua": "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
-    #             "sec-ch-ua-mobile": "?0",
-    #             "sec-ch-ua-platform": "macOS"
-    #             }
-
-    # response2 = requests.post(login_url2, data=login_data, headers=headers2)
-
-    # cookies2 = response2.cookies
-
-    # print(cookies2)
-
-    # parse 'cookies2' to get the value of the '.ASPXFORMSAUTH' cookie and set it to the 'aspxformsauth' variable
-    # aspxformsauth = cookies2['.ASPXFORMSAUTH']
 
     # next we need to get the current user's ID
     # we can do this by making a GET request to 'https://www.stoningtoncountryclub.com/api/v1/GetCurrentUser'
