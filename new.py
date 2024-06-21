4f3539e715c5442d877749bd4c3c813d2a91f10c
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-06-20 22:43:47 -0400

    selenium

--- new.py
+++ new.py
@@ -28,28 +28,53 @@ def getTeeTime():
     driver = webdriver.Chrome()
 
 
-    login_data = {
-        'p$lt$PageContent$pageplaceholder$p$lt$zoneRight$CHOLogin$LoginControl$ctl00$Login1$UserName': username,
-        'p$lt$PageContent$pageplaceholder$p$lt$zoneRight$CHOLogin$LoginControl$ctl00$Login1$Password': password,
-        'p$lt$PageContent$pageplaceholder$p$lt$zoneRight$CHOLogin$LoginControl$ctl00$Login1$LoginButton': 'Login',
-    }
+    # login_data = {
+        # 'p$lt$PageContent$pageplaceholder$p$lt$zoneRight$CHOLogin$LoginControl$ctl00$Login1$UserName': username,
+        # 'p$lt$PageContent$pageplaceholder$p$lt$zoneRight$CHOLogin$LoginControl$ctl00$Login1$Password': password,
+        # 'p$lt$PageContent$pageplaceholder$p$lt$zoneRight$CHOLogin$LoginControl$ctl00$Login1$LoginButton': 'Login',
+    # }
 
     login_url = 'https://www.stoningtoncountryclub.com/login.aspx'
 
-    response = requests.post(login_url, data=login_data)
+    # response = requests.post(login_url, data=login_data)
 
-    cookies = response.cookies
+    # cookies = response.cookies
+
+    driver.get(login_url)
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'p_lt_PageContent_pageplaceholder_p_lt_zoneRight_CHOLogin_LoginControl_ctl00_Login1_UserName')))
+
+    # enter username and password
+    driver.find_element(By.ID, 'p_lt_PageContent_pageplaceholder_p_lt_zoneRight_CHOLogin_LoginControl_ctl00_Login1_UserName').send_keys(username)
+    driver.find_element(By.ID, 'p_lt_PageContent_pageplaceholder_p_lt_zoneRight_CHOLogin_LoginControl_ctl00_Login1_Password').send_keys(password)
+
+    # click login button
+    driver.find_element(By.ID, 'p_lt_PageContent_pageplaceholder_p_lt_zoneRight_CHOLogin_LoginControl_ctl00_Login1_LoginButton').click()
+
+    # translate cookies to be used by requests lib
+    selenium_cookies = driver.get_cookies()
+    cookies_dict = {}
+    for cookie in selenium_cookies:
+        name = cookie['name']
+        value = cookie['value']
+        cookies_dict[name] = value
+
+    cookies = json.load(cookies_dict)
 
     print(cookies)
 
     emailupdate_url = 'https://www.stoningtoncountryclub.com/UserEmailUpdate.aspx'
 
-    response15 = requests.get(emailupdate_url, cookies=cookies, data=login_data)
+    # response15 = requests.get(emailupdate_url, cookies=cookies, data=login_data)
+    print()
+    currentUser = requests.get(getcurrentuser_url, cookies=cookies)
+    print()
+    print(currentUser.text())
 
-    print(cookies)
+    # print(cookies)
 
     # list the tee times
-    response2 = requests.get('https://www.stoningtoncountryclub.com/api/v1/teetimes/GetAvailableTeeTimes/20230517/1548/0/null/false', cookies=cookies)
+    # response2 = requests.get('https://www.stoningtoncountryclub.com/api/v1/teetimes/GetAvailableTeeTimes/20230517/1548/0/null/false', cookies=cookies)
     # print(response2.text)
 
     # get the current user's ID
@@ -92,3 +117,5 @@ def getTeeTime():
     # response3 = requests.post(booking_url, data=dataToSend, cookies=cookies)
 
     # print(response3.text)
+
+getTeeTime()
