1c5e6335629dd6ae77844bbb086c7f5e4e5e38b6
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-06-28 11:16:50 -0400

    config defaults to 6am book for 8am on weekends

--- book.py
+++ book.py
@@ -1,9 +1,9 @@
 # Config
-waitForHour = False # wait for the hour, ex: if the script runs at 5:58 and is set to wait for the hour, it will wait for 6AM
-waitForWeekend = False # only book on weekends (saturday and sunday)
-timeToBook = '14:30' # what time the script should book, ex: 08:30 for 8:30AM
+waitForHour = True # wait for the hour, ex: if the script runs at 5:58 and is set to wait for the hour, it will wait for 6AM
+waitForWeekend = True # only book on weekends (saturday and sunday)
+timeToBook = '08:00' # what time the script should book, ex: 08:30 for 8:30AM
 daysAfter = 7
-waitForRunTime = False
+waitForRunTime = True
 runTimeH = 5 # when the script should run, ex: 6 for 6:00 AM. only applicable if waitForRunTime is enabled
 runTimeM = 58 # ex: 58 for 5:58 AM, keep this two minutes before to allow time to login. only applicable if waitForRunTime is enabled
 
