52695ff1995192a43b173749601ef76f5c0341b4
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-08-31 16:14:10 -0400

    import from config

--- book.py
+++ book.py
@@ -1,11 +1,4 @@
-# Config
-waitForHour = True # wait for the hour, ex: if the script runs at 5:58 and is set to wait for the hour, it will wait for 6AM
-waitForWeekend = True # only book on weekends (saturday and sunday)
-timeToBook = '8:00' # what time the script should book, ex: 08:30 for 8:30AM
-daysAfter = 7
-waitForRunTime = True
-runTimeH = 5 # when the script should run, ex: 6 for 6:00 AM. only applicable if waitForRunTime is enabled
-runTimeM = 58 # ex: 58 for 5:58 AM, keep this two minutes before to allow time to login. only applicable if waitForRunTime is enabled
+from config import waitForHour, waitForWeekend, timeToBook, daysAfter, waitForRunTime, runTimeH, runTimeM
 
 import requests
 import json
